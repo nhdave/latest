4cf17788b06868ffd067b770caa2ad6a80ac2c18
Dave Crockett <nhdave@comcast.net>
2017-02-01 19:17:33 -0500

    First changes

--- resources/views/categories/credentials.blade.php
+++ resources/views/categories/credentials.blade.php
@@ -58,5 +58,6 @@
     <div align="center">
         <a class="btn btn-success" href="{{ route('credentials.create') }}"><i class="glyphicon glyphicon-plus"></i> Create New Credential</a><br>
         <a class="btn btn-link" href="{{ route('categories.index') }}"><i class="glyphicon glyphicon-backward"></i> Back</a>
-    </div>  
+    </div> 
+    @parent 
 @stop
